24f7982f96123f49b59bcac2bc8dbc31003ba544
eunse <eunseokim43@gmail.com>
2025-12-18 18:36:40 +0900

    initiate commit

---
+++ build.gradle.kts
@@ -0,0 +1,55 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.5.8"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "dev.es"
+version = "0.0.1-SNAPSHOT"
+description = "MyAsset"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // web
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    // OAuth2 & Security
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    testImplementation("org.springframework.security:spring-security-test")
+
+    // JPA
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+    // mysql
+    runtimeOnly("com.mysql:mysql-connector-j")
+
+    // docker
+    runtimeOnly("org.springframework.boot:spring-boot-docker-compose")
+
+    // lombok
+    implementation("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+
+    // test
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    // devtool
+    "developmentOnly"("org.springframework.boot:spring-boot-devtools")
+
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ src/test/java/dev/es/myasset/MyAssetApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.es.myasset;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAssetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
